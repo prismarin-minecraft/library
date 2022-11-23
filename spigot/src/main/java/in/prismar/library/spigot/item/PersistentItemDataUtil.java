d11611884a50f658a548279449c29d07d7534cd3
ReaperMaga <reapermaga@gmail.com>
2022-11-23 07:48:29 +0100

    Moved ParticleUtil and PersistentItemDataUtil from game-plugin to library

---
+++ spigot/src/main/java/in/prismar/library/spigot/item/PersistentItemDataUtil.java
@@ -0,0 +1,46 @@
+package in.prismar.library.spigot.item;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+public final class PersistentItemDataUtil {
+
+    private static final Map<String, NamespacedKey> CACHED_KEYS = new HashMap<>();
+
+    public static void setInteger(Plugin plugin, ItemStack stack, String key, int value) {
+        ItemMeta meta = stack.getItemMeta();
+        PersistentDataContainer container = meta.getPersistentDataContainer();
+        if(!CACHED_KEYS.containsKey(key)) {
+            CACHED_KEYS.put(key, new NamespacedKey(plugin, key));
+        }
+        container.set(CACHED_KEYS.get(key), PersistentDataType.INTEGER, value);
+        stack.setItemMeta(meta);
+
+    }
+
+    public static int getInteger(Plugin plugin, ItemStack stack, String key) {
+        ItemMeta meta = stack.getItemMeta();
+        PersistentDataContainer container = meta.getPersistentDataContainer();
+        if(!CACHED_KEYS.containsKey(key)) {
+            CACHED_KEYS.put(key, new NamespacedKey(plugin, key));
+        }
+        NamespacedKey namespacedKey = CACHED_KEYS.get(key);
+        if(container.has(namespacedKey, PersistentDataType.INTEGER)) {
+            return container.get(namespacedKey, PersistentDataType.INTEGER);
+        }
+        return 0;
+    }
+}
---
+++ spigot/src/main/java/in/prismar/library/spigot/particle/ParticleUtil.java
@@ -0,0 +1,37 @@
+package in.prismar.library.spigot.particle;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+public final class ParticleUtil {
+
+    public static void spawnParticleAlongLine(Location start, Location end, Particle particle, int pointsPerLine, int particleCount) {
+        double d = start.distance(end) / pointsPerLine;
+        for (int i = 0; i < pointsPerLine; i++) {
+            Location l = start.clone();
+            Vector direction = end.toVector().subtract(start.toVector()).normalize();
+            Vector v = direction.multiply(i * d);
+            l.add(v.getX(), v.getY(), v.getZ());
+            start.getWorld().spawnParticle(particle, l, particleCount);
+        }
+    }
+
+    public static void spawnParticleAlongLine(Player player, Location start, Location end, Particle particle, int pointsPerLine, int particleCount) {
+        double d = start.distance(end) / pointsPerLine;
+        for (int i = 0; i < pointsPerLine; i++) {
+            Location l = start.clone();
+            Vector direction = end.toVector().subtract(start.toVector()).normalize();
+            Vector v = direction.multiply(i * d);
+            l.add(v.getX(), v.getY(), v.getZ());
+            player.spawnParticle(particle, l, particleCount);
+        }
+    }
+}
