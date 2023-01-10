f27feec52061088648df0e0b9c7ddd469c36ef24
ReaperMaga <reapermaga@gmail.com>
2023-01-10 13:11:18 +0100

    Added double digit formatter

--- common/src/main/java/in/prismar/library/common/math/NumberFormatter.java
+++ common/src/main/java/in/prismar/library/common/math/NumberFormatter.java
@@ -1,6 +1,24 @@
 package in.prismar.library.common.math;
 
+import java.text.DecimalFormat;
+
 public final class NumberFormatter {
+
+    private static final DecimalFormat TWO_DIGITS_FORMAT = new DecimalFormat("0.00");
+    private static final DecimalFormat THREE_DIGITS_FORMAT = new DecimalFormat("0.000");
+    private static final DecimalFormat ONE_DIGIT_FORMAT = new DecimalFormat("0.0");
+
+    public static String formatDoubleToThreeDigits(double value) {
+        return THREE_DIGITS_FORMAT.format(value);
+    }
+
+    public static String formatDoubleToTwoDigits(double value) {
+        return TWO_DIGITS_FORMAT.format(value);
+    }
+
+    public static String formatDoubleToOneDigits(double value) {
+        return ONE_DIGIT_FORMAT.format(value);
+    }
 
     /**
      * Format number value to thousands
