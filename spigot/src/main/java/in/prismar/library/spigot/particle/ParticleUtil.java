5fe6e96e869e35481ddac9f221c32e6e6ba25c6c
ReaperMaga <reapermaga@gmail.com>
2023-01-18 18:26:13 +0100

    Added another particle method

--- spigot/src/main/java/in/prismar/library/spigot/particle/ParticleUtil.java
+++ spigot/src/main/java/in/prismar/library/spigot/particle/ParticleUtil.java
@@ -3,6 +3,7 @@ package in.prismar.library.spigot.particle;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Consumer;
 import org.bukkit.util.Vector;
 
 /**
@@ -21,6 +22,17 @@ public final class ParticleUtil {
             Vector v = direction.multiply(i * d);
             l.add(v.getX(), v.getY(), v.getZ());
             start.getWorld().spawnParticle(particle, l, particleCount);
+        }
+    }
+
+    public static void spawnParticleAlongLine(Location start, Location end, int pointsPerLine, Consumer<Location> consumer) {
+        double d = start.distance(end) / pointsPerLine;
+        for (int i = 0; i < pointsPerLine; i++) {
+            Location l = start.clone();
+            Vector direction = end.toVector().subtract(start.toVector()).normalize();
+            Vector v = direction.multiply(i * d);
+            l.add(v.getX(), v.getY(), v.getZ());
+            consumer.accept(l);
         }
     }
 
